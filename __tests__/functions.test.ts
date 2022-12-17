7bdb3f6b6eab68a13e7896f6a9905ec82995cea1
Taryn King <marshaeking@gmail.com>
2022-12-17 09:41:33 -0600

    add equality test for functions.test.ts

--- __tests__/functions.test.ts
+++ __tests__/functions.test.ts
@@ -8,11 +8,15 @@ describe('shuffleArray should', () => {
         expect(Array.isArray(result)).toBeTruthy()
     })
 
-    test('have same length as original array', () => {
+    test('have same length as test array', () => {
         expect(result.length).toEqual(testArr.length)
     })
 
     test('same items are in returned array', () => {
         expect(result).toEqual(expect.arrayContaining(testArr))
+    })
+
+    test('not be in the same order as test array', () => {
+        expect(result.join('')).not.toEqual(testArr.join(''))
     })
 })
